e45c72aa4f6f2af4dceb11a8067d08cbaa303dda
sonu1239 <ss316742@gmail.com>
2021-10-12 15:00:27 +0530

    LineComprison-UC1

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComprison
+{
+    
+        class Program
+        {
+            static double distance(int x1, int y1, int x2, int y2)
+            {
+                // Calculating distance
+                return Math.Sqrt(Math.Pow(x2 - x1, 2) +
+                              Math.Pow(y2 - y1, 2) * 1.0);
+            }
+
+            //Driver code
+            public static void Main(string[] args)
+            {
+                Console.WriteLine(Math.Round(distance(7, 3, 3, 7)
+                                           * 100000.0) / 100000.0);
+                Console.ReadLine();
+            }
+
+        }
+}
+
+
+
+
+
+
