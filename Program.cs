57d374b83314f188d1145d37286856879012d2ae
sonu1239 <ss316742@gmail.com>
2021-10-20 00:18:48 +0530

    Line comprison using class method

--- Program.cs
+++ Program.cs
@@ -9,10 +9,9 @@ namespace LineComprison
     
         class Program
     {
-        static void Main(string[] args)
+        public static double CompareLine()
         {
-
-            //varibles declaration
+            //varibles
             double x1, x2, y1, y2;
             double len, length;
             double point1, point2;
@@ -21,34 +20,34 @@ namespace LineComprison
             double len1, length1;
             double point3, point4;
 
-            //taking user inputs for Line 1
-            Console.WriteLine("Enter the coordinates of line number 1");
+            //taking user input of line 1
+            Console.WriteLine("Enter the coordinates for line number 1");
             Console.WriteLine();
 
-            Console.WriteLine("Enter value of x1:");
-            x1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter value of y2:");
+            Console.WriteLine("Enter x1 of line1");
+            x1 = Convert.ToDouble(Console.ReadLine()); //Converting string to double
+            Console.WriteLine("Enter y1 of line1");
             y1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter value of x2:");
+            Console.WriteLine("Enter x2 of line1");
             x2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter value of y2:");
+            Console.WriteLine("Enter y2 of line1");
             y2 = Convert.ToDouble(Console.ReadLine());
 
-            //Taking user inputs for Line 2
+            //taking user inputs of line 2
             Console.WriteLine();
-            Console.WriteLine("Enter the coordinates of line number 2");
+            Console.WriteLine("Enter the coordinates for line number 2");
             Console.WriteLine();
 
-            Console.WriteLine("Enter value of x1:");
+            Console.WriteLine("Enter x1 of line2");
             x11 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter value of y1");
+            Console.WriteLine("Enter y1 of line2");
             y11 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter value of x2:");
+            Console.WriteLine("Enter x2 of line2");
             x22 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter value of y2:");
+            Console.WriteLine("Enter y2 of line2");
             y22 = Convert.ToDouble(Console.ReadLine());
 
-            //using the math class methods to calculate the sqaures and square root as per the formula
+            //using the math class methods pow and sqrt for calculation
             // formula of length = sqrt[(x2 -x1)^2 + (y2-y1)^2]
             point1 = Math.Pow((x2 - x1), 2);
             point2 = Math.Pow((y2 - y1), 2);
@@ -64,7 +63,7 @@ namespace LineComprison
             Console.WriteLine("The length of line 2 : " + length1);
             Console.WriteLine();
 
-            if (length == length1)
+            if (length == length1)                    // if statement for comparision of the two lines
             {
                 Console.WriteLine("Both the lines are equal");
             }
@@ -76,6 +75,13 @@ namespace LineComprison
             {
                 Console.WriteLine("Line 2 is greater than line 1 ");
             }
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            CompareLine();
+
         }
     }
+    
 }
